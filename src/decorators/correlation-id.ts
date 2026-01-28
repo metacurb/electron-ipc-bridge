ed1e95d0f68bf6a372de0f5ec347bff7d617ea2c
BeauAgst <10343831+metacurb@users.noreply.github.com>
2026-01-28 11:51:18 +0000

    delete CorrelationId decorator, it confuses the mental model

--- src/decorators/correlation-id.ts
+++
@@ -1,5 +0,0 @@
-import { getCorrelationId } from "../correlation/get-correlation-id";
-
-import { createParamInjector } from "./utils/create-param-injector";
-
-export const CorrelationId = createParamInjector(() => getCorrelationId());
