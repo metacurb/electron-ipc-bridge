381fd3431b0c4a5b63861b4d3c269f8fb0ff5f39
BeauAgst <10343831+metacurb@users.noreply.github.com>
2026-02-03 12:33:06 +0000

    handles invalid or duplicate decorators

--- src/decorators/utils/create-ipc-handler-decorator.spec.ts
+++ src/decorators/utils/create-ipc-handler-decorator.spec.ts
@@ -27,4 +27,24 @@ describe("createIpcHandlerDecorator", () => {
       type: "handle",
     });
   });
+
+  test("should throw if multiple decorators applied to same method", () => {
+    expect(() => {
+      class TestController {
+        @TestDecorator()
+        @TestDecorator()
+        methodName() {}
+      }
+    }).toThrow("Method 'methodName' already has an IPC decorator.");
+  });
+
+  test("should throw if applied to a non-method", () => {
+    expect(() => {
+      class TestController {
+        // @ts-expect-error This is a method decorator, but we're validating it anyway
+        @TestDecorator()
+        notAMethod = "string";
+      }
+    }).toThrow("IPC decorators can only be applied to methods.");
+  });
 });
--- src/decorators/utils/create-ipc-handler-decorator.ts
+++ src/decorators/utils/create-ipc-handler-decorator.ts
@@ -3,6 +3,10 @@ import { IpcHandlerType, PendingHandlerMetadata } from "../../metadata/types";
 
 export const createIpcHandlerDecorator = (type: IpcHandlerType) => (): MethodDecorator => {
   return (target, propertyKey, descriptor: PropertyDescriptor) => {
+    if (typeof descriptor?.value !== "function") {
+      throw new Error(`IPC decorators can only be applied to methods.`);
+    }
+
     const handlerMeta: PendingHandlerMetadata = {
       handler: descriptor.value,
       methodName: String(propertyKey),
@@ -11,6 +15,10 @@ export const createIpcHandlerDecorator = (type: IpcHandlerType) => (): MethodDec
 
     const pending: PendingHandlerMetadata[] =
       Reflect.getMetadata(IPC_PENDING_HANDLERS, target) || [];
+
+    if (pending.some((h) => h.methodName === String(propertyKey))) {
+      throw new Error(`Method '${String(propertyKey)}' already has an IPC decorator.`);
+    }
 
     pending.push(handlerMeta);
 
