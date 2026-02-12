c026505d5b344efa51bea00e1c392e6830c3a21c
BeauAgst <10343831+metacurb@users.noreply.github.com>
2026-02-12 16:43:04 +0000

    resolve CodeQL warning

--- packages/shared/src/utils/to-snake-case.ts
+++ packages/shared/src/utils/to-snake-case.ts
@@ -2,6 +2,6 @@ export const toSnakeCase = (str: string): string =>
   str
     .trim()
     .replace(/[\s-]+/g, "_")
-    .replace(/([A-Z]+)([A-Z][a-z])/g, "$1_$2")
+    .replace(/(?<=[A-Z])(?=[A-Z][a-z])/g, "_")
     .replace(/([a-z\d])([A-Z])/g, "$1_$2")
     .toLocaleLowerCase();
